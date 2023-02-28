7100116f93df8d5b1b585271ce62e2bbde44a0e2
tonyap92 <114396404+tonyap92@users.noreply.github.com>
2023-02-28 12:24:43 +0300

    Moved the questions to a separate file questions.js

---
+++ questions.js
@@ -0,0 +1,114 @@
+const questions = [
+  {
+    question: "Sweet and watery",
+    options: [
+      {
+        text: "Onion",
+        image: "./img/onion.png",
+      },
+      {
+        text: "Carrot",
+        image: "./img/carrot.png",
+      },
+      {
+        text: "Cabbage",
+        image: "./img/cabbage.png",
+      },
+      {
+        text: "Grapes",
+        image: "./img/grapes.png",
+      },
+    ],
+    answer: "Grapes",
+  },
+  {
+    question: "Small and labial",
+    options: [
+      {
+        text: "Accordion",
+        image: "./img/accordion.png",
+      },
+      {
+        text: "Guitar",
+        image: "./img/guitar.png",
+      },
+      {
+        text: "Hormone",
+        image: "./img/hormone.png",
+      },
+      {
+        text: "Trumpet",
+        image: "./img/trumpet.png",
+      },
+    ],
+    answer: "Hormone",
+  },
+  {
+    question: "Rubber and for swimming",
+    options: [
+      {
+        text: "Spanking",
+        image: "./img/spanking.png",
+      },
+      {
+        text: "Beach",
+        image: "./img/beach.png",
+      },
+      {
+        text: "Flippers",
+        image: "./img/flippers.png",
+      },
+      {
+        text: "Umbrella",
+        image: "./img/umbrella.png",
+      },
+    ],
+    answer: "Flippers",
+  },
+  {
+    question: "On batteries",
+    options: [
+      {
+        text: "Baseball cap",
+        image: "./img/baseballcap.png",
+      },
+      {
+        text: "Pen",
+        image: "./img/pen.png",
+      },
+      {
+        text: "Scissors",
+        image: "./img/scissors.png",
+      },
+      {
+        text: "Lantern",
+        image: "./img/lantern.png",
+      },
+    ],
+    answer: "Lantern",
+  },
+  {
+    question: "Sharp for leveling",
+    options: [
+      {
+        text: "Pitchfork",
+        image: "./img/pitchfork.png",
+      },
+      {
+        text: "Saw",
+        image: "./img/saw.png",
+      },
+      {
+        text: "Shovel",
+        image: "./img/shovel.png",
+      },
+      {
+        text: "Spatula",
+        image: "./img/spatula.png",
+      },
+    ],
+    answer: "Spatula",
+  },
+];
+
+
